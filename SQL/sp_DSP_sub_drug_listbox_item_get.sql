d3c4886efbeea265eb6ad0eca4740c06e45caec0
turtletse <tsechunfai772@hotmail.com>
2015-01-02 16:24:47 +0800

    SUB DRUG

---
+++ SQL/sp_DSP_sub_drug_listbox_item_get.sql
@@ -0,0 +1,19 @@
+DROP procedure if EXISTS sp_DSP_sub_drug_listbox_item_get;
+delimiter $$
+CREATE PROCEDURE sp_DSP_sub_drug_listbox_item_get (
+	IN in_drug_id INT, IN in_incl_not_specified INT)
+BEGIN
+	DROP TEMPORARY TABLE IF EXISTS result;
+	CREATE TEMPORARY TABLE result (sub_drug_id int, item_id VARCHAR(20), item_name VARCHAR(255));
+    IF (in_incl_not_specified>0) THEN
+		INSERT INTO result VALUES (0, CONCAT_WS('||', in_drug_id, '0'), '-');
+    END IF;
+    INSERT INTO result
+		SELECT sub_drug_id, CONCAT_WS('||', drug_id, sub_drug_id), sub_drug_name
+        FROM master_sub_drug_list
+        WHERE drug_id = in_drug_id;
+	SELECT item_id, item_name FROM result ORDER BY sub_drug_id;
+    DROP TEMPORARY TABLE result;
+END $$
+delimiter ;
+-- CALL sp_DSP_sub_drug_listbox_item_get (101001, 1);
