11d13b8505a230e1f3b1d8e076cb205b1305babc
turtletse <tsechunfai772@hotmail.com>
2015-01-08 00:32:44 +0800

    Pat Reg GUI

---
+++ cmcms/CMCMS/PatientRegistration.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace CMCMS
+{
+    partial class PatientRegistration
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.radioButton_female = new System.Windows.Forms.RadioButton();
+            this.radioButton_male = new System.Windows.Forms.RadioButton();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton_IDNo = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton_passportNo = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.drugSelectionPanel_allergic = new CMCMS.DrugSelectionPanel();
+            this.checkBox_g6pd = new System.Windows.Forms.CheckBox();
+            this.textBox_patReg_chiName = new System.Windows.Forms.TextBox();
+            this.textBox_patReg_engName = new System.Windows.Forms.TextBox();
+            this.textBox_patReg_HKID = new System.Windows.Forms.TextBox();
+            this.textBox_patReg_phoneNo = new System.Windows.Forms.TextBox();
+            this.dateTimePicker_DOB = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_selectAllergicDrug = new System.Windows.Forms.Button();
+            this.button_removeSelectedDrug = new System.Windows.Forms.Button();
+            this.listBox_selectedAllergicDrug = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "新登記";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.dateTimePicker_DOB);
+            this.groupBox1.Controls.Add(this.textBox_patReg_phoneNo);
+            this.groupBox1.Controls.Add(this.textBox_patReg_HKID);
+            this.groupBox1.Controls.Add(this.textBox_patReg_engName);
+            this.groupBox1.Controls.Add(this.textBox_patReg_chiName);
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(9, 45);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1354, 150);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "個人資料";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.radioButton_female);
+            this.panel2.Controls.Add(this.radioButton_male);
+            this.panel2.Location = new System.Drawing.Point(651, 73);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(120, 20);
+            this.panel2.TabIndex = 11;
+            // 
+            // radioButton_female
+            // 
+            this.radioButton_female.AutoSize = true;
+            this.radioButton_female.Location = new System.Drawing.Point(61, -2);
+            this.radioButton_female.Name = "radioButton_female";
+            this.radioButton_female.Size = new System.Drawing.Size(43, 24);
+            this.radioButton_female.TabIndex = 1;
+            this.radioButton_female.TabStop = true;
+            this.radioButton_female.Text = "女";
+            this.radioButton_female.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_male
+            // 
+            this.radioButton_male.AutoSize = true;
+            this.radioButton_male.Location = new System.Drawing.Point(0, -2);
+            this.radioButton_male.Name = "radioButton_male";
+            this.radioButton_male.Size = new System.Drawing.Size(43, 24);
+            this.radioButton_male.TabIndex = 0;
+            this.radioButton_male.TabStop = true;
+            this.radioButton_male.Text = "男";
+            this.radioButton_male.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(596, 73);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 20);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "性別: ";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton_IDNo);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.radioButton_passportNo);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(600, 34);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(226, 27);
+            this.panel1.TabIndex = 9;
+            // 
+            // radioButton_IDNo
+            // 
+            this.radioButton_IDNo.AutoSize = true;
+            this.radioButton_IDNo.Location = new System.Drawing.Point(0, 0);
+            this.radioButton_IDNo.Name = "radioButton_IDNo";
+            this.radioButton_IDNo.Size = new System.Drawing.Size(75, 24);
+            this.radioButton_IDNo.TabIndex = 2;
+            this.radioButton_IDNo.TabStop = true;
+            this.radioButton_IDNo.Text = "身份證";
+            this.radioButton_IDNo.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 2);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "或";
+            // 
+            // radioButton_passportNo
+            // 
+            this.radioButton_passportNo.AutoSize = true;
+            this.radioButton_passportNo.Location = new System.Drawing.Point(112, 0);
+            this.radioButton_passportNo.Name = "radioButton_passportNo";
+            this.radioButton_passportNo.Size = new System.Drawing.Size(59, 24);
+            this.radioButton_passportNo.TabIndex = 4;
+            this.radioButton_passportNo.TabStop = true;
+            this.radioButton_passportNo.Text = "護照";
+            this.radioButton_passportNo.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(177, 2);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "號碼: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 112);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 20);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "住址: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(223, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(151, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "出生日期 (日/月/年): ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "聯絡電話: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(223, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "英文姓名: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "中文姓名: ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox_selectedAllergicDrug);
+            this.groupBox2.Controls.Add(this.button_removeSelectedDrug);
+            this.groupBox2.Controls.Add(this.button_selectAllergicDrug);
+            this.groupBox2.Controls.Add(this.checkBox_g6pd);
+            this.groupBox2.Controls.Add(this.drugSelectionPanel_allergic);
+            this.groupBox2.Location = new System.Drawing.Point(9, 201);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1354, 446);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "藥物敏感史/病史";
+            // 
+            // drugSelectionPanel_allergic
+            // 
+            this.drugSelectionPanel_allergic.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.drugSelectionPanel_allergic.Location = new System.Drawing.Point(7, 68);
+            this.drugSelectionPanel_allergic.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.drugSelectionPanel_allergic.Name = "drugSelectionPanel_allergic";
+            this.drugSelectionPanel_allergic.Size = new System.Drawing.Size(829, 371);
+            this.drugSelectionPanel_allergic.TabIndex = 0;
+            // 
+            // checkBox_g6pd
+            // 
+            this.checkBox_g6pd.AutoSize = true;
+            this.checkBox_g6pd.Location = new System.Drawing.Point(10, 36);
+            this.checkBox_g6pd.Name = "checkBox_g6pd";
+            this.checkBox_g6pd.Size = new System.Drawing.Size(72, 24);
+            this.checkBox_g6pd.TabIndex = 1;
+            this.checkBox_g6pd.Text = "G6PD";
+            this.checkBox_g6pd.UseVisualStyleBackColor = true;
+            // 
+            // textBox_patReg_chiName
+            // 
+            this.textBox_patReg_chiName.Location = new System.Drawing.Point(93, 31);
+            this.textBox_patReg_chiName.Name = "textBox_patReg_chiName";
+            this.textBox_patReg_chiName.Size = new System.Drawing.Size(124, 26);
+            this.textBox_patReg_chiName.TabIndex = 12;
+            // 
+            // textBox_patReg_engName
+            // 
+            this.textBox_patReg_engName.Location = new System.Drawing.Point(310, 31);
+            this.textBox_patReg_engName.Name = "textBox_patReg_engName";
+            this.textBox_patReg_engName.Size = new System.Drawing.Size(269, 26);
+            this.textBox_patReg_engName.TabIndex = 13;
+            // 
+            // textBox_patReg_HKID
+            // 
+            this.textBox_patReg_HKID.Location = new System.Drawing.Point(846, 33);
+            this.textBox_patReg_HKID.Name = "textBox_patReg_HKID";
+            this.textBox_patReg_HKID.Size = new System.Drawing.Size(269, 26);
+            this.textBox_patReg_HKID.TabIndex = 14;
+            // 
+            // textBox_patReg_phoneNo
+            // 
+            this.textBox_patReg_phoneNo.Location = new System.Drawing.Point(93, 70);
+            this.textBox_patReg_phoneNo.Name = "textBox_patReg_phoneNo";
+            this.textBox_patReg_phoneNo.Size = new System.Drawing.Size(124, 26);
+            this.textBox_patReg_phoneNo.TabIndex = 15;
+            // 
+            // dateTimePicker_DOB
+            // 
+            this.dateTimePicker_DOB.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker_DOB.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker_DOB.Location = new System.Drawing.Point(380, 70);
+            this.dateTimePicker_DOB.Name = "dateTimePicker_DOB";
+            this.dateTimePicker_DOB.Size = new System.Drawing.Size(199, 26);
+            this.dateTimePicker_DOB.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1053, 26);
+            this.textBox1.TabIndex = 17;
+            // 
+            // button_selectAllergicDrug
+            // 
+            this.button_selectAllergicDrug.Location = new System.Drawing.Point(843, 166);
+            this.button_selectAllergicDrug.Name = "button_selectAllergicDrug";
+            this.button_selectAllergicDrug.Size = new System.Drawing.Size(75, 99);
+            this.button_selectAllergicDrug.TabIndex = 2;
+            this.button_selectAllergicDrug.Text = ">>";
+            this.button_selectAllergicDrug.UseVisualStyleBackColor = true;
+            // 
+            // button_removeSelectedDrug
+            // 
+            this.button_removeSelectedDrug.Location = new System.Drawing.Point(843, 295);
+            this.button_removeSelectedDrug.Name = "button_removeSelectedDrug";
+            this.button_removeSelectedDrug.Size = new System.Drawing.Size(75, 99);
+            this.button_removeSelectedDrug.TabIndex = 3;
+            this.button_removeSelectedDrug.Text = "<<";
+            this.button_removeSelectedDrug.UseVisualStyleBackColor = true;
+            // 
+            // listBox_selectedAllergicDrug
+            // 
+            this.listBox_selectedAllergicDrug.FormattingEnabled = true;
+            this.listBox_selectedAllergicDrug.ItemHeight = 20;
+            this.listBox_selectedAllergicDrug.Location = new System.Drawing.Point(924, 130);
+            this.listBox_selectedAllergicDrug.Name = "listBox_selectedAllergicDrug";
+            this.listBox_selectedAllergicDrug.Size = new System.Drawing.Size(130, 304);
+            this.listBox_selectedAllergicDrug.TabIndex = 4;
+            // 
+            // PatientRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "PatientRegistration";
+            this.Size = new System.Drawing.Size(1380, 654);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton_IDNo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton_passportNo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton radioButton_female;
+        private System.Windows.Forms.RadioButton radioButton_male;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private DrugSelectionPanel drugSelectionPanel_allergic;
+        private System.Windows.Forms.TextBox textBox_patReg_HKID;
+        private System.Windows.Forms.TextBox textBox_patReg_engName;
+        private System.Windows.Forms.TextBox textBox_patReg_chiName;
+        private System.Windows.Forms.CheckBox checkBox_g6pd;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_DOB;
+        private System.Windows.Forms.TextBox textBox_patReg_phoneNo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox_selectedAllergicDrug;
+        private System.Windows.Forms.Button button_removeSelectedDrug;
+        private System.Windows.Forms.Button button_selectAllergicDrug;
+    }
+}
